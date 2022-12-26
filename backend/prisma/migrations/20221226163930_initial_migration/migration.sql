9f69458f81ef2d39584cccd3770eedc79558f5a6
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2022-12-26 17:39:48 +0100

    initial-migration

---
+++ backend/prisma/migrations/20221226163930_initial_migration/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "ShoppingList" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "totalPrice" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "ShoppingListItem" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "totalPrice" REAL NOT NULL,
+    "shoppingListId" INTEGER NOT NULL,
+    "checked" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "ShoppingListItem_shoppingListId_fkey" FOREIGN KEY ("shoppingListId") REFERENCES "ShoppingList" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
